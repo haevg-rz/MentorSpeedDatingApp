5944043daae5fe0214504895dcd2300d0b5edbd0
Enrico Pau <32924566+enricopau@users.noreply.github.com>
2020-03-12 14:50:05 +0100

    Initialer Commit ref #12

---
+++ MentorSpeedDatingApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.RightsManagement;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MentorSpeedDatingApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        #region RelayCommands
+
+        #endregion
+
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
