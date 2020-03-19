4ebd0853db6779ec6ad1650772942f61dcd03409
Enrico Pau <32924566+enricopau@users.noreply.github.com>
2020-03-19 09:53:37 +0100

    Ungenutzte Controls gelöscht WIP Version DataGrid - Logik muss angepasst werden! ValidationRules müssen dem Nutzer sinnvoll angezeigt werden. ref #1

--- MentorSpeedDatingApp/MainWindow.xaml.cs
+++ MentorSpeedDatingApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MentorSpeedDatingApp.Models;
+using MentorSpeedDatingApp.ViewModel;
 
 namespace MentorSpeedDatingApp
 {
@@ -23,6 +26,26 @@ namespace MentorSpeedDatingApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DataGrid_OnRowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                IPerson person = e.Row.DataContext as IPerson;
+                person.Save();
+            }
+        }
+
+        private void DataGrid_OnPreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == DataGrid.DeleteCommand)
+            {
+                if (MessageBox.Show("Sind Sie sicher, dass sie die Personen löschen wollen?", "Bitte bestätigen!", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                {
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
--- MentorSpeedDatingApp/Models/Person.cs
+++ MentorSpeedDatingApp/Models/Person.cs
@@ -1,10 +1,14 @@
-﻿namespace MentorSpeedDatingApp.Models
+﻿using System.Collections;
+
+namespace MentorSpeedDatingApp.Models
 {
     public interface IPerson
     {
         string Titel { get; set; }
         string Name { get; set; }
         string Vorname { get; set; }
+        void Save();
+        void Delete();
     }
 
     public class Mentee : IPerson
@@ -12,6 +16,14 @@
         public string Titel { get; set; }
         public string Name { get; set; }
         public string Vorname { get; set; }
+
+        public void Save()
+        {
+        }
+
+        public void Delete()
+        {
+        }
     }
 
     public class Mentor : IPerson
@@ -19,5 +31,13 @@
         public string Titel { get; set; }
         public string Name { get; set; }
         public string Vorname { get; set; }
+
+        public void Save()
+        {
+        }
+
+        public void Delete()
+        {
+        }
     }
 }
