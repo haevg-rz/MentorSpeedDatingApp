a9e0c880cf14f83d823fa47c5cefa93dbe13d4a1
Enrico Pau <32924566+enricopau@users.noreply.github.com>
2020-03-24 10:34:08 +0100

    Update for Person.cs

--- MentorSpeedDatingApp/Models/Person.cs
+++ MentorSpeedDatingApp/Models/Person.cs
@@ -7,8 +7,6 @@ namespace MentorSpeedDatingApp.Models
         string Titel { get; set; }
         string Name { get; set; }
         string Vorname { get; set; }
-        void Save();
-        void Delete();
     }
 
     public class Mentee : IPerson
@@ -16,14 +14,6 @@ namespace MentorSpeedDatingApp.Models
         public string Titel { get; set; }
         public string Name { get; set; }
         public string Vorname { get; set; }
-
-        public void Save()
-        {
-        }
-
-        public void Delete()
-        {
-        }
     }
 
     public class Mentor : IPerson
@@ -32,12 +22,5 @@ namespace MentorSpeedDatingApp.Models
         public string Name { get; set; }
         public string Vorname { get; set; }
 
-        public void Save()
-        {
-        }
-
-        public void Delete()
-        {
-        }
     }
 }
