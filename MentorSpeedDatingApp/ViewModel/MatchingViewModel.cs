5a964b617a0f35e7c8330222a7f4da6c54a08bfa
Enrico Pau <32924566+enricopau@users.noreply.github.com>
2020-04-07 11:47:29 +0200

    ref #2

---
+++ MentorSpeedDatingApp/ViewModel/MatchingViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using GalaSoft.MvvmLight;
+using MentorSpeedDatingApp.Models;
+
+namespace MentorSpeedDatingApp.ViewModel
+{
+    public class MatchingViewModel : ViewModelBase
+    {
+        public ObservableCollection<MatchingModel> Matchings { get; set; }
+
+        public MatchingViewModel()
+        {
+            if (this.IsInDesignMode)
+            {
+                this.Matchings = new ObservableCollection<MatchingModel>()
+                {
+                    new MatchingModel()
+                    {
+                        MatchedMentee = new Mentee()
+                            {Name = "TestName1", Titel = "TestTitel1", Vorname = "TestVorname1"},
+                        MatchedTime = new DateTime(2020, 4, 7, 7, 30, 0),
+                        MatchedMentor = new Mentor()
+                            {Name = "TestName1", Titel = "TestTitel1", Vorname = "TestVorname1"}
+                    },
+                    new MatchingModel()
+                    {
+                        MatchedMentee = new Mentee()
+                            {Name = "TestName2", Titel = "TestTitel2", Vorname = "TestVorname2"},
+                        MatchedTime = new DateTime(2020, 4, 7, 8, 0, 0),
+                        MatchedMentor = new Mentor()
+                            {Name = "TestName2", Titel = "TestTitel2", Vorname = "TestVorname2"}
+                    },
+                    new MatchingModel()
+                    {
+                        MatchedMentee = new Mentee()
+                            {Name = "TestName3", Titel = "TestTitel3", Vorname = "TestVorname3"},
+                        MatchedTime = new DateTime(2020, 4, 7, 8, 30, 0),
+                        MatchedMentor = new Mentor()
+                            {Name = "TestName3", Titel = "TestTitel3", Vorname = "TestVorname3"}
+                    },
+                    new MatchingModel()
+                    {
+                        MatchedMentee = new Mentee()
+                            {Name = "TestName4", Titel = "TestTitel4", Vorname = "TestVorname4"},
+                        MatchedTime = new DateTime(2020, 4, 7, 9, 0, 0),
+                        MatchedMentor = new Mentor()
+                            {Name = "TestName4", Titel = "TestTitel4", Vorname = "TestVorname4"}
+                    }
+                };
+            }
+
+            ;
+        }
+    }
+}
+
+public class MatchingModel : ViewModelBase
+{
+    private Mentor matchedMentor;
+
+    public Mentor MatchedMentor
+    {
+        get => this.matchedMentor;
+        set => base.Set(ref this.matchedMentor, value);
+    }
+
+    private Mentee matchedMentee;
+
+    public Mentee MatchedMentee
+    {
+        get => this.matchedMentee;
+        set => base.Set(ref this.matchedMentee, value);
+    }
+
+    private DateTime matchedTime;
+
+    public DateTime MatchedTime
+    {
+        get => this.matchedTime;
+        set => base.Set(ref this.matchedTime, value);
+    }
+}
---
+++ MentorSpeedDatingApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MentorSpeedDatingApp.ViewModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace MentorSpeedDatingApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            var userDecision = MainViewModel.OnCloseCommand();
+
+            if (userDecision == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            ViewModelLocator.Cleanup();
+        }
+    }
+}
